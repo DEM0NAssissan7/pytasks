1a9033be7ba05739c976b788200c2e1a456025d2
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2024-09-04 20:17:46 -0500

    initial work

---
+++ example.py
@@ -0,0 +1,108 @@
+from tasks import *
+
+needsroot()
+
+# Redefine some commands for the sake of scalability
+prerun("""
+function apt {
+    /usr/bin/apt --assume-yes $@
+}
+function flatpak {
+    /usr/bin/flatpak -y $@
+}
+function ask_prompt {
+    # Argument 1: Message
+    # Argument 2: If yes (function name)
+    # 3: If no (function name)
+    # 4: Default
+
+    message=$1
+    yes=$2
+    no=$3
+
+    default=$yes
+    p="[Y/n]"
+
+    if [ -z $4 ]; then
+        true
+    elif [ $4 == "n" ]; then
+        default=$no
+        p="[y/N]"
+    fi
+
+
+    read -p "$message $p: " input
+    if [ -z $input ]; then
+        $default
+    elif [ ${input,,} == "y" -o ${input,,} == "yes" ]; then
+        $yes
+    elif [ ${input,,} == "n" -o ${input,,} == "no" ]; then
+        $no
+    else
+        ask_prompt $@
+    fi
+}
+
+""")
+
+
+
+Task("Flatpak w/ Flathub (app repository)","""
+            apt install flatpak plasma-discover-backend-flatpak
+            flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+            """, reboot=True, selected=True)
+Task("ZRAM (enhancement)", """
+            apt install zram-tools
+            echo -e "ALGO=zstd\nPERCENT=65\nPRIORITY=100" | tee -a /etc/default/zramswap
+            service zramswap reload
+            """, selected=True)
+Task("Command Not Found (enhancement)","""
+            apt install command-not-found
+            apt update
+            """, selected=True)
+Task("Pipewire w/ Wireplumber (enhancement)",
+            "apt install pipewire-audio pipewire-jack",
+            reboot=True, selected=True)
+
+# Scripts
+Task("KDE Debloat (script)", """
+            apps=
+
+            function rmpkg {
+                apps="$apps $@"
+            }
+            # Libreoffice
+            
+            rmpkg libreoffice-*
+            
+            # Random KDE apps
+            rmpkg kontrast 
+     
+            apt remove $apps
+            apt autoremove
+            apt autoclean
+""")
+
+# Apps
+Task("VLC (app)",
+            "apt install vlc")
+Task("mpv (app)",
+            "apt install mpv",
+            selected=True)
+
+# Controversial
+Task("OnlyOffice (app)",
+            "flatpak install app/org.onlyoffice.desktopeditors/x86_64/stable")
+Task("Google Chrome (app)","""
+            echo "Google Chrome terms of service: https://policies.google.com/terms https://www.google.com/chrome/terms"
+            echo "Would you like to install Google Chrome? By installing Google Chrome, you agree to its terms of service."
+            ask_prompt "Do you want to install Google Chrome?" true exit
+
+            chrome_pkg="google-chrome-stable_current_amd64.deb"
+            wget https://dl.google.com/linux/direct/$chrome_pkg
+            apt install ./$chrome_pkg
+            echo "Cleaning $PWD/$chrome_pkg"
+            rm $chrome_pkg
+            """)
+
+run_tasks()
---
+++ tasks.py
@@ -0,0 +1,173 @@
+#!/bin/python3
+from enum import Enum
+
+import subprocess
+import os
+from sys import argv
+
+# Ensure the program has root permissions
+def needsroot():
+    if os.getuid() != 0:
+        print("This program must be run as root.")
+        exit()
+
+# General functions
+def prompt(message, default):
+    yes_no_prompt = "[Y/n]"
+    _default = True
+    if default == False:
+        yes_no_prompt = "[y/N]"
+        _default = False
+    i = input(message + " " + yes_no_prompt + ": ").lower()
+
+    if i == "":
+        return _default
+
+    if i == "y" or i == "yes":
+        return True
+
+    if i == "n" or i == "no":
+        return False
+
+def new_section(title):
+    longline = "<------------------------------>"
+    print("\n" + longline)
+    print(title)
+    print(longline + "\n")
+
+def finished_section(title):
+    longline = "------>"
+    print("\n" + longline + " Finished task '" + title + "'\n")
+
+
+# Tasks
+tasks = []
+prerun_script = ""
+shell_exec = "/bin/sh"
+
+def prerun(script):
+    global prerun_script
+    prerun_script = script
+
+system_needs_reboot=False
+class Task:
+    name="Task"
+    script="echo Task"
+    selected=True
+    reboot=False
+    def __init__(self, name, script, *args, **kwargs):
+        self.name = name
+        self.script = script
+        self.reboot = kwargs.get("reboot", False)
+        self.selected = kwargs.get("selected", False)
+        tasks.append(self)
+
+    def run(self):
+        new_section("Running task '" + self.name + "'...")
+        subprocess.run(prerun_script + self.script, shell=True, executable=shell_exec)
+        finished_section(self.name)
+        # If a service needs to reboot, we prompt the user at the end to reboot if they want
+        global system_needs_reboot
+        system_needs_reboot = system_needs_reboot or self.reboot
+    
+    def get_selected_status(self):
+        if self.selected == True:
+            return "X"
+        if self.selected == False:
+            return " "
+    
+    def enable(self):
+        self.selected = True
+    
+    def disable(self):
+        self.selected = False
+    
+    def toggle(self):
+        if self.selected == True:
+            self.disable()
+        elif self.selected == False:
+            self.enable()
+        return self.selected
+
+# User task selection
+def show_tasks():
+    i = 0
+    for task in tasks:
+        i = i + 1
+        print("[" + task.get_selected_status() + "] ("+str(i)+"): " + task.name)
+
+def show_prompt_info():
+    os.system("clear")
+    print()
+    print("Tasks:")
+    show_tasks()
+    print()
+    print("Enter number (e.g '1' or '2') to toggle selection")
+    print()
+    print("You may specify multiple tasks separated by a space (e.g. '1 2 5 6')")
+    print()
+    print("To enable all tasks, input 'e'")
+    print("To disable all tasks, input 'd'")
+    print()
+    print("To QUIT, input 'q'")
+    print()
+
+def prompt_task_selection():
+    show_prompt_info()
+    while True:
+        i = input("Selection (or type 'i' to begin installation): ").lower()
+        if "e" in i:
+            for task in tasks:
+                task.enable()
+            show_prompt_info()
+        elif "d" in i:
+            for task in tasks:
+                task.disable()
+            show_prompt_info()
+        elif "q" in i:
+            quit_program()
+            break
+        elif "i" in i:
+            break
+        else:
+            i = i.split(" ")
+            try:
+                for num in i:
+                    tasks[int(num) - 1].toggle()
+                show_prompt_info()
+            except:
+                continue
+
+def run_selected_tasks():
+    for task in tasks:
+        if task.selected == True:
+            task.run()
+
+def finalize():
+    for i in range(5):
+        print()
+    new_section("Tasks completed!")
+    if system_needs_reboot == True:
+        prompt_reboot()
+
+def quit_program():
+    for i in range(5):
+        print()
+    print("Exiting...")
+    exit()
+    
+def prompt_reboot():
+    # os.system("clear")
+    i = prompt("To finalize the installation, would you like to restart your computer?", "n")
+    if i == True:
+        os.system("systemctl reboot")
+        
+
+
+# The last thing that runs in the config
+def run_tasks():
+    prompt_task_selection()
+    # After user made selection
+    run_selected_tasks()
+    finalize()
+    quit_program()
