94dff5cd019602c8839b8326251f8259d8d99f81
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2024-09-09 12:55:37 -0500

    add xwayland fractional scaling

--- fedora.py
+++ fedora.py
@@ -45,22 +45,57 @@ prerun("""
     }
 """)
 
+default_select(True)
+
 Task("Fedora Linux Flathub Removal (repository)",
-     "flatpak remote-delete fedora", selected=True)
+    "flatpak remote-delete fedora")
+
+Task("GNOME Xwayland fractional scaling (unstable)", """
+    dnf copr enable taaem/mutter-xwayland-fractional-scaling
+
+    dnf rei gnome-settings-daemon
+    dnf up mutter mutter-common gnome-settings-daemon
+
+    gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer', 'xwayland-native-scaling', 'variable-refresh-rate']"
+    """, reboot=True)
 
 Task("Flatpak by Default", """
     gsettings set org.gnome.software packaging-format-preference "['flatpak', 'rpm']"
     killall gnome-software
-    """, selected=True)
+    """)
 
 Task("Gnome Console (replaces gnome-terminal)", """
     dnf rm gnome-terminal gnome-terminal-nautilus
     dnf in gnome-console
-    """, selected=True)
+    """)
 
 # This implies that Fedora flatpak repo has already been removed
 Task("mpv (app)",
-     "flatpak install app/io.mpv.Mpv/x86_64/stable", selected=True)
+    "flatpak install app/io.mpv.Mpv/x86_64/stable")
+
+Task("Matlab", """
+    function matrun {
+        toolbox run --container matlab $@
+    }
+    
+    # Shit ton of dependencies
+    
+    toolbox create matlab --distro rhel --release 8.10
+    matrun sudo dnf -y install alsa-lib.x86_64 cairo.x86_64 cairo-gobject.x86_64 cups-libs.x86_64 \
+    gdk-pixbuf2.x86_64 glib2.x86_64 glibc.x86_64 glibc-langpack-en.x86_64 glibc-locale-source.x86_64 \
+    gtk3.x86_64 libICE.x86_64 libXcomposite.x86_64 libXcursor.x86_64 libXdamage.x86_64 libXfixes.x86_64 \
+    libXft.x86_64 libXinerama.x86_64 libXrandr.x86_64 libXt.x86_64 libXtst.x86_64 libXxf86vm.x86_64 \
+    libcap.x86_64 libdrm.x86_64 libglvnd-glx.x86_64 libsndfile.x86_64 libtool-ltdl.x86_64 libuuid.x86_64 \
+    libwayland-client.x86_64 make.x86_64 mesa-libgbm.x86_64 net-tools.x86_64 nspr.x86_64 nss.x86_64 \
+    nss-util.x86_64 pam.x86_64 pango.x86_64 procps-ng.x86_64 sudo.x86_64 unzip.x86_64 which.x86_64 zlib.x86_64
+    
+    
+    
+    
+    """, selected=False)
+
+Task("Google Chrome",
+    "dnf in google-chrome-stable")
 
 
 run_tasks()
